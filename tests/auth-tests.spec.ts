ab580276c088dc5474156b419f488daecbd5da90
Ana-Gavriliuc <gavrilyuk-1994@mail.ru>
2025-12-11 19:49:20 +0100

    complete HW16 (Faker, APP_URL env, CI, README)

--- tests/auth-tests.spec.ts
+++ tests/auth-tests.spec.ts
@@ -1,8 +1,17 @@
 import {expect, test} from "@playwright/test";
+import {faker} from "@faker-js/faker/locale/ar";
 
-const appUrl = "https://fe-delivery.tallinn-learning.ee/signin";
+const appUrl: string =
+    process.env.APP_URL ||
+    "https://fe-delivery.tallinn-learning.ee/signin";
+
+let randomUsername: string;
+let randomPassword: string;
 
 test.beforeEach(async ({ page }) => {
+    randomUsername = faker.internet.username();
+    randomPassword = faker.internet.password();
+
     await page.goto(appUrl);
     await page.waitForLoadState("networkidle");
 });
@@ -12,7 +21,7 @@ test('Login button is disabled if one field is empty', async ({ page }) => {
     const signInButton = page.getByTestId("signIn-button");
 
     await expect(signInButton).toBeEnabled();
-    await loginField.fill("test");
+    await loginField.fill(randomUsername);
     await expect(signInButton).toBeDisabled();
 });
 
@@ -22,8 +31,8 @@ test('Auth error modal is visible if credentials are wrong', async ({ page }) =>
     const signInButton = page.getByTestId("signIn-button");
     const authErrorPopup = page.getByTestId("authorizationError-popup");
 
-    await loginField.fill("test");
-    await passwordField.fill("fwerfhguewrygferg");
+    await loginField.fill(randomUsername);
+    await passwordField.fill(randomPassword);
     await signInButton.click();
 
     await expect(authErrorPopup).toBeVisible();
